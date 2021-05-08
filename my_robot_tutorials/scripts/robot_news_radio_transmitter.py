5224b4db57427e51cd8ea5e0d67c1be38c3f4377
toritani3 <typej9griffon.asura@gmail.com>
2021-05-08 18:56:40 +0900

    initial commit

---
+++ my_robot_tutorials/scripts/robot_news_radio_transmitter.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+if  __name__  ==  "__main__":
+    rospy.init_node('node_news_radio_transmitter')
+
+    pub = rospy.Publisher("/robot_news_radio", String, queue_size=1)
+
+    rate = rospy.Rate(2)
+    while not rospy.is_shutdown():
+        msg = String()
+        msg.data = "Hi, this is Sean from the Robot News Radio"
+        pub.publish(msg)
+        rate.sleep
+
+    rospy.loginfo("Node was stopped")
